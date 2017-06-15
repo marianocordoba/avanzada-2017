02011025f9acbd7ce2b3799dea72197b968cf661
Mariano Córdoba <marianocordoba039@gmail.com>
2017-06-15 03:14:30 -0300

    Práctico 8 completo

--- Prácticos/Funciones.hs
+++ Prácticos/Funciones.hs
@@ -34,3 +34,66 @@ isAsc :: Ord a => [a] -> Bool
 isAsc [] = True
 isAsc [x] = True
 isAsc (x:xs) = x < head xs && isAsc xs
+
+-------------------------------------------------
+
+-- Ejercicio 3
+
+minList :: [Int] -> Int
+minList [] = maxBound
+minList (x:xs) = x `min` (minList xs)
+
+-------------------------------------------------
+
+-- Ejercicio 4
+equalSum :: [Int] -> Bool
+equalSum xs = equalSum' xs 0
+
+equalSum' :: [Int] -> Int -> Bool
+equalSum' [] _ = False
+equalSum' (x:xs) n = x == n + sum(xs) || equalSum' xs (x + n)
+
+-------------------------------------------------
+
+-- Ejercicio 5
+
+isSegment :: Eq a => [a] -> [a] -> Bool
+isSegment [] ys = True
+isSegment (x:xs) [] = False
+isSegment (x:xs) (y:ys) = (x == y && isSegment' xs ys) || isSegment (x:xs) ys
+
+isSegment' :: Eq a => [a] -> [a] -> Bool
+isSegment' [] ys = True
+isSegment' (x:xs) [] = False
+isSegment' (x:xs) (y:ys) = x == y && isSegment' xs ys
+
+-------------------------------------------------
+
+-- Ejercicio 6
+
+isSumSegment :: [Int] -> Bool
+isSumSegment xs = isSumSegment' 0 xs
+
+isSumSegment' :: Int -> [Int] -> Bool
+isSumSegment' n [] = n == 0
+isSumSegment' n (x:xs) = x + (sum xs) == n || isSumSegment' (x + n) xs 
+
+-------------------------------------------------
+
+-- Ejercicio 7
+
+minDif :: [Int] -> [Int] -> Int
+minDif [] ys = maxBound
+minDif xs [] = maxBound
+minDif (x:xs) (y:ys) = (abs (x - y)) `min` minDif xs ys
+
+-------------------------------------------------
+
+-- Ejercicio 8
+
+countEvenOdd :: [Int] -> (Int, Int)
+countEvenOdd [] = (0,0)
+countEvenOdd (x:xs) 
+    | x `mod` 2 == 0 = (1 + a, b)
+    | otherwise = (a, 1 + b)
+    where (a, b) = countEvenOdd xs
